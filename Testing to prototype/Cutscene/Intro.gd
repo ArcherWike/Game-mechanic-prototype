cd6626081f0af5fd18dd6ee4a286210306d799f8
Wiktoria <wiktoria.szczepanska.x@gmail.com>
2022-04-06 21:31:40 +0200

    added cutscene scene
    here you can interact with the tv and turn on the cutscene.

--- Testing to prototype/Cutscene/Intro.gd
+++ Testing to prototype/Cutscene/Intro.gd
@@ -1,4 +1,5 @@
-extends Node2D
+extends VideoPlayer
+
 
 
 # Declare member variables here. Examples:
---
+++ Testing to prototype/Cutscene/cutscene.gd
@@ -0,0 +1,49 @@
+extends KinematicBody2D
+
+onready var _animated_sprite = $AnimatedSprite
+onready var colision = $CollisionShape2D
+ 
+func _input(event):
+	if event.is_action_pressed("interact") and Isplayer == true:
+		open_door()
+
+var opened = false
+var interact = false
+var Isplayer = false
+
+func _on_AnimatedSprite_animation_finished():
+	_animated_sprite.stop()
+	if opened:
+		_animated_sprite.set_frame(3)
+		colision.disabled = true
+	else:
+		_animated_sprite.set_frame(0)
+
+func open_door():
+	opened = true
+	interact = true
+	_animated_sprite.set_frame(0)
+	_animated_sprite.play()
+	
+func col_false():
+	colision.disabled = false
+
+func _on_Area2D_body_exited(body):
+	if body.name == "Player" and interact == true:
+		opened = false
+		_animated_sprite.set_frame(3)
+		call_deferred("col_false")		
+		_animated_sprite.play("door", true)
+		interact = false
+		Isplayer = false
+		
+
+#func _on_Area2D_body_entered(body):
+#	if body.name == "Player":
+#		if Input.is_action_pressed("interact"):
+	#		open_door()
+
+
+func _on_Area2D_body_entered(body):
+	if body.name == "Player":
+		Isplayer = true
